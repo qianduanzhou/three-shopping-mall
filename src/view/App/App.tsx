2c21c4a81071e8f388d387d1287a44d54012c34d
周泓滨 <zhouhongbin@163.com>
2022-03-31 16:25:56 +0800

    init: 初始化项目

---
+++ src/view/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import './css/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="test">111</div>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
