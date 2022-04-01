6d524b58794dcbb821ec0adc8153556c7188b6f9
周泓滨 <zhouhongbin@163.com>
2022-04-01 12:00:37 +0800

    feat(配置): 路由增加相关配置

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/view/home/view/detail/index.tsx
+++ src/view/home/view/detail/index.tsx
@@ -1,8 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams, useLocation } from "react-router-dom";
+
+interface LocationState {
+    key: number
+}
+
 function Detail() {
     let params = useParams();
+    let location = useLocation();
+    let state = location.state as LocationState;
+    let [searchParams, setSearchParams] = useSearchParams();
     return (
-        <div className="list">Detail/{params.id}</div>
+        <div className="list">Detail/params={params.id},query={searchParams.get("type")},state={state.key}</div>
     )
 }
 export default Detail;
