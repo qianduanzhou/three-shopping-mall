891960dfe5209e9f767593b5ac619216a7b91aab
周泓滨 <zhouhongbin@163.com>
2022-04-02 17:44:15 +0800

    feat(首页): 导航栏字段修改

--- src/components/topNav/index.tsx
+++ src/components/topNav/index.tsx
@@ -12,7 +12,7 @@ interface NavItem {
 	text: string
 }
 export default function TopNav() {
-	let [navObj, setNavObj] = useState<Nav>({//导航栏对象
+	let [nav, setNav] = useState<Nav>({//导航栏对象
 		left: [{//左侧
 			id: 0,
 			text: "首页"
@@ -32,24 +32,24 @@ export default function TopNav() {
 	function getNavClass(id: number) {//获取导航栏类
 		return cx({
 			[styles.nav]: true,
-			actived: id === navObj.activedId
+			actived: id === nav.activedId
 		})
 	}
 	function clickNav(id: number) {//点击导航栏
-		setNavObj({
-			...navObj,
+		setNav({
+			...nav,
 			activedId: id
 		})
 	}
 	return (
 		<div className={styles.container}>
 			<div className={styles.left}>
-				{navObj.left.map(v => {
+				{nav.left.map(v => {
 					return <div className={getNavClass(v.id)} key={v.id} onClick={() => clickNav(v.id)}>{v.text}</div>
 				})}
 			</div>
 			<div className={styles.right}>
-				{navObj.right.map(v => {
+				{nav.right.map(v => {
 					return <div className={getNavClass(v.id)} key={v.id} onClick={() => clickNav(v.id)}>{v.text}</div>
 				})}
 			</div>
